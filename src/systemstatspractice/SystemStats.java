b7e263b679aaa30717aa163634658e47ced919c9
robsarro <robsarro@gmail.com>
2016-09-11 17:11:16 -0400

    Added Functionality for date and time

---
+++ src/systemstatspractice/SystemStats.java
@@ -0,0 +1,16 @@
+
+package systemstatspractice;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rob
+ */
+public class SystemStats {
+    public static Date currentDateTime(){
+        Date dateobj = new Date();
+        return dateobj;
+    }
+    
+}
